8dad8c4be413d6d1abb0db7522d0e5dddbe10dac
Hussein039 <84193192+Hussein039@users.noreply.github.com>
2022-08-01 18:30:55 -0400

    Update Contact.js

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Contact = () => {
   return (
     <div>
-      <h1><i className="fab fa-youtube">d</i></h1>
+      <h1><i className="fab fa-youtube"></i></h1>
     </div>
   );
 };
